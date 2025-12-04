1f85416219cee9b2c238d3b681e5d95b63e7502f
Jules <robliquejules@gmail.com>
2025-12-04 16:00:12 +0100

    Création de l'échiquier
    Ajout des pions et de l'échequier adapté à l'écran

--- src/main.c
+++ src/main.c
@@ -3,12 +3,18 @@
 
 
 // Gestionnaire de texture
+<<<<<<< Updated upstream
 Texture2D gTileTextures[3];
 int gTileTextureCount = 0;
+=======
+Texture2D gTileTextures[14];  // <-- IMPORTANT : il faut 14 textures, pas 3 !
+int gTileTextureCount = 14;
+>>>>>>> Stashed changes
 
 
 int main(void)
 {
+<<<<<<< Updated upstream
     const int screenWidth = BOARD_COLS * TILE_SIZE;
     const int screenHeight = BOARD_ROWS * TILE_SIZE;
 
@@ -21,6 +27,32 @@ int main(void)
     gTileTextures[2] = LoadTexture("assets/tool.png");
     gTileTextureCount = 3;
     
+=======
+    // Taille du moniteur
+    int screenW = GetMonitorWidth(0);
+    int screenH = GetMonitorHeight(0);
+
+    InitWindow(screenW, screenH, "Raylib Board Game");
+    ToggleFullscreen();
+    SetTargetFPS(60);
+
+    // Chargement des textures
+    gTileTextures[0] = LoadTexture("assets/carreau_blanc.png");
+    gTileTextures[1] = LoadTexture("assets/carreau_noir.png");
+    gTileTextures[2] = LoadTexture("assets/cavalier_blanc.png");
+    gTileTextures[3] = LoadTexture("assets/cavalier_noir.png");
+    gTileTextures[4] = LoadTexture("assets/fou_blanc.png");
+    gTileTextures[5] = LoadTexture("assets/fou_noir.png");
+    gTileTextures[6] = LoadTexture("assets/pion_blanc.png");
+    gTileTextures[7] = LoadTexture("assets/pion_noir.png");
+    gTileTextures[8] = LoadTexture("assets/reine_blanche.png");
+    gTileTextures[9] = LoadTexture("assets/reine_noir.png");
+    gTileTextures[10] = LoadTexture("assets/roi_blanc.png");
+    gTileTextures[11] = LoadTexture("assets/roi_noir.png");
+    gTileTextures[12] = LoadTexture("assets/tour_blanche.png");
+    gTileTextures[13] = LoadTexture("assets/tour_noir.png");
+
+>>>>>>> Stashed changes
     Board board = {0};
     GameInit(&board);
 
@@ -35,9 +67,6 @@ int main(void)
 
         GameDraw(&board);
         DrawFPS(10, 10);
-
-        double time = GetTime();
-        DrawText(TextFormat("Time : %.2f", time), 170, 10, 20, GREEN);
 
         EndDrawing();
     }
